f871de82014a78bb594779ec22f75e5483f306ef
gagabd3 <133128836+gagabd3@users.noreply.github.com>
2023-05-10 17:05:39 +0300

    gree

---
+++ homebridge-gree-air-conditioner-master/src/locale/en.ts
@@ -0,0 +1,17 @@
+import { LocaleMessages } from '../settings';
+
+const messages: LocaleMessages = {
+  power: 'Power',
+  mode: 'Mode',
+  verticalSwing: 'Vertical Swing',
+  horizontalSwing: 'Horizontal Swing',
+  quietMode: 'Quiet Mode',
+  powerfulMode: 'Powerful Mode',
+  light: 'Light',
+  fanMode: 'Fan Mode',
+  dryMode: 'Dry Mode',
+  fanSpeed: 'Fan Speed',
+  sleepMode: 'Sleep Mode',
+};
+
+export default messages;
